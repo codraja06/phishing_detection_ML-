c3f447b808e7b3e8f2ce73be41200daa316f1e5b
codraja06 <rajarock1006@gmail.com>
2025-10-20 18:43:50 +0530

    Updated

---
+++ model/model.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+import pickle
+
+df=pd.read_csv("phishing.csv")
+X=df.drop('class',axis=1)
+y=df['class']
+
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42)
+
+model=RandomForestClassifier()
+model.fit(X_train,y_train)
+
+with open("phishing.pkl","wb") as file:
+  pickle.dump(model,file)
